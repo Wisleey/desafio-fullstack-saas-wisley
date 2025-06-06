f2302044f394c7e298951376a551d1608d741f9e
wisleey <wisley_killat@hotmail.com>
2025-06-06 11:43:06 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: true,
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: true,
+    },
+    define: {
+      "process.env": env,
+    },
+  };
+});
